29bd12c6aa36c6371d8dd4ce8de00fb2cbe83532
JohnnySun <bmy001@gmail.com>
2026-07-14 01:42:44 +0800

    settings: keep Android 16 bring-up controls fail closed

--- src/com/odin2/odinsettings/tiles/KeepScreenOnTileService.java
+++ src/com/odin2/odinsettings/tiles/KeepScreenOnTileService.java
@@ -1,4 +1,16 @@
 package com.odin2.odinsettings.tiles;
 
-public final class KeepScreenOnTileService extends FailClosedTileService {
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+
+public final class KeepScreenOnTileService extends TileService {
+    @Override
+    public void onStartListening() {
+        Tile tile = getQsTile();
+        if (tile == null) {
+            return;
+        }
+        tile.setState(Tile.STATE_UNAVAILABLE);
+        tile.updateTile();
+    }
 }
